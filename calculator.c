6fbb50277ee8fd90c07bd1d93afb65687b0a908b
Abir Howladar <abirhowlader922@gmail.com>
2025-07-29 08:37:32 +0600

    Add files via upload

---
+++ calculator.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main()
+{
+    double num1,num2 ;
+    char op;
+//    printf("Enter two number : ");
+//    scanf("%lf%lf",&num1&num2);
+
+    printf("Enter an operator(+,-,*,/) : ");
+    scanf("%c",&op);
+     printf("Enter two number : ");
+    scanf("%lf%lf",&num1,&num2);
+
+    switch(op)
+    {
+    case '+':
+        {
+            printf("%lf+%lf=%lf\n",num1,num2,num1+num2);
+            break;
+        }
+     case '-':
+        {
+            printf("%lf-%lf=%lf\n",num1,num2,num1-num2);
+            break;
+        }
+
+     case '*':
+        {
+            printf("%lf*%lf=%lf\n",num1,num2,num1*num2);
+            break;
+        }
+     case '/':
+        {
+            printf("%lf/%lf=%lf\n",num1,num2,num1/num2);
+            break;
+        }
+    }
+}
